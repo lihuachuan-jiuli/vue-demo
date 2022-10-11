da88bd1da79365279b8a5b584dd8779e7b054c7f
jiu-Li <2920460143@qq.com>
2022-10-11 18:10:48 +0800

    练习了vue

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue' //引入vue对象, 类似于<script src="vue.js"></script>
+import App from './App.vue' //App.vue文件里的对象引入过来(vue项目的页面入口)
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false //在控制有一句提示消息
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+new Vue({  //开始实例化vue
+  render: h => h(App), //固定语法 准备渲染App页面
+}).$mount('#app')   //固定语法 渲染到index,html 文件里 id 叫 app 的标签上
+ 
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,13 @@
+//  vue脚手架项目 - 默认的配置文件名
+// webpack + node环境 - 导出配置项
+// CommonJS 规范
+module.exports = {
+  devServer: {
+    port: 3000, //端口号
+    open: true, //浏览器自动打开
+  },
+}
+
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
